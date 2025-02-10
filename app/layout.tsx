3648d263c45832392cd6580551eea7208a2145e2
HuynhMinhHai79 <huynhhai13979@gmail.com>
2025-02-10 13:09:03 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Poppins } from "next/font/google";
-
 const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200", "400", "700", "900"] })
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,32 @@ import { Portfolio } from "@/components/Portfolio"
 import { KeyMetrics } from "@/components/KeyMetrics";
 import { Services } from "@/components/Services";
 import { Contact } from "@/components/Contact";
+import pool from "@/ultils/db";
 
-export default function Home() {
+
+
+const Home = () => {
+  const fectchDatafromDB = async () => {
+    try {
+      const client = await pool.connect();
+      console.log('Connected to DB');
+      const result = await client.query('SELECT * FROM public.users');
+      const data = result.rows;
+      console.log('Data fetched from DB:', data);
+      client.release();
+      return data;
+    } catch (error) {
+      console.error('Error fetching data from DB:', error);
+      throw error;
+    }
+  }
+ fectchDatafromDB()
+ .then((data)=>{  
+  console.log('Data fetched from DB:', data);
+ })
+ .catch((error)=>{
+  console.error('Error fetching data from DB:', error);
+ })
   return (
     <>
       <Navbar />
@@ -21,3 +45,4 @@ export default function Home() {
     </>
   );
 }
+export default Home;
