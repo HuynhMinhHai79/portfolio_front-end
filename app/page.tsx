af9d0f6a816c337e725558dbcc208c6083e7b226
HuynhMinhHai79 <huynhhai13979@gmail.com>
2025-02-13 10:53:24 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -11,27 +11,29 @@ import pool from "@/ultils/db";
 
 
 const Home = () => {
-  const fectchDatafromDB = async () => {
-    try {
-      const client = await pool.connect();
-      console.log('Connected to DB');
-      const result = await client.query('SELECT * FROM public.users');
-      const data = result.rows;
-      console.log('Data fetched from DB:', data);
-      client.release();
-      return data;
-    } catch (error) {
-      console.error('Error fetching data from DB:', error);
-      throw error;
-    }
+  const fetchDataFromDB = async () => {
+      try {
+          const client = await pool.connect();
+          console.log('Connected to DB');
+          const result = await client.query('SELECT * FROM public.users');
+          const data = result.rows;
+          console.log('Data fetched from DB:', data);
+          client.release();
+          return data;
+      } catch (error) {
+          console.error('Error fetching data from DB:', error);
+          throw error;
+      }
   }
- fectchDatafromDB()
- .then((data)=>{  
-  console.log('Data fetched from DB:', data);
- })
- .catch((error)=>{
-  console.error('Error fetching data from DB:', error);
- })
+
+  fetchDataFromDB()
+      .then((data) => {
+          console.log('Data fetched from DB:', data);
+      })
+      .catch((error) => {
+          console.error('Error fetching data from DB:', error);
+      });
+
   return (
     <>
       <Navbar />
